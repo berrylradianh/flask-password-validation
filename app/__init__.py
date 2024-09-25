2d719158f9d1bd5fff37e9f3976a2dfacebcf919
berrylradianh <berrylhamesha@gmail.com>
2024-09-26 05:28:00 +0700

    feat: remove max length password

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,5 @@ with open('.env.json') as config_file:
 app.config['HOST'] = config['host']
 app.config['PORT'] = config['port']
 app.config['PASSWORD_MIN_LENGTH'] = config['password_min_length']
-app.config['PASSWORD_MAX_LENGTH'] = config['password_max_length']
 
 from app import routes
